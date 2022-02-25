b273e34a848b613b5544e433d344255660cf1e65
Dakota <ramensea0@gmail.com>
2022-02-24 16:45:36 -0800

    Added in fetching arg names from a CSV

--- StringValueCreator.go
+++ StringValueCreator.go
@@ -1,31 +1,30 @@
 package main
 
 import (
-	"os"
 	"encoding/csv"
 	"io"
+	"os"
 	"strings"
 )
 
-const MIN_SIZE_OF_SPREAD_SHEET  = 3
-const CSV_POSITION_KEY  = 0
-const CSV_POSITION_VALUE  = 1
-const CSV_POSITION_TRANSLATABLE  = 2
-const CSV_POSITION_ARG_NAMES  = 3
+const MIN_SIZE_OF_SPREAD_SHEET = 3
+const CSV_POSITION_KEY = 0
+const CSV_POSITION_VALUE = 1
+const CSV_POSITION_TRANSLATABLE = 2
+const CSV_POSITION_ARG_NAMES = 3
 
-func getStringKeys(languageId string, config *StringCheeseConfig,  platformProcessor func(value string, argNames string, sv *StringValue)) (*StringKeys, error) {
+func getStringKeys(languageId string, config *StringCheeseConfig, platformProcessor func(value string, argNames string, sv *StringValue)) (*StringKeys, error) {
 	if config.shouldUseSpreadSheetForStrings {
-		return GetStringKeysFromCSV(languageId,config,platformProcessor)
+		return GetStringKeysFromCSV(languageId, config, platformProcessor)
 	} else {
-		return GetStringKeysFromXML(languageId,config,platformProcessor)
+		return GetStringKeysFromXML(languageId, config, platformProcessor)
 	}
 }
 
-
-func GetStringKeysFromCSV(languageId string, config *StringCheeseConfig,  platformProcessor func(value string, argNames string, sv *StringValue)) (*StringKeys, error) {
+func GetStringKeysFromCSV(languageId string, config *StringCheeseConfig, platformProcessor func(value string, argNames string, sv *StringValue)) (*StringKeys, error) {
 
 	usingLanguageId := languageId
-	if languageId == LANGUAGE_ID_NONE{
+	if languageId == LANGUAGE_ID_NONE {
 		usingLanguageId = config.rootLanguageIdToUse
 	}
 
@@ -41,7 +40,7 @@ func GetStringKeysFromCSV(languageId string, config *StringCheeseConfig,  platfo
 	var index = -1
 	for {
 		index++
-		line,err := reader.Read()
+		line, err := reader.Read()
 
 		if err == io.EOF {
 			break
@@ -74,7 +73,13 @@ func GetStringKeysFromCSV(languageId string, config *StringCheeseConfig,  platfo
 			translatable = true
 		}
 
-		values.strings[key] = CreateStringValue(translatable, key, value, "", config, platformProcessor)
+		argNames := ""
+
+		if len(line) > CSV_POSITION_ARG_NAMES {
+			argNames = line[CSV_POSITION_ARG_NAMES]
+		}
+
+		values.strings[key] = CreateStringValue(translatable, key, value, argNames, config, platformProcessor)
 
 	}
 
